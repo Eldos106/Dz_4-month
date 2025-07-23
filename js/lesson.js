dcd1edd140cb55ccb9bdeb3ac7c56125cf805f95
Eldos106 <toktosunoveldos99@gmail.com>
2025-07-23 23:17:33 +0600

    DZ6_4-month

--- js/lesson.js
+++ js/lesson.js
@@ -143,10 +143,64 @@ const converter = async (element, targetElement, targetElement2) => {
             targetElement2.value = '';
         }
     } catch (error) {
-        console.error(error);
+        console.error( error);
     }
 };
 
 somInput.addEventListener('input', () => converter(somInput, usdInput, euroInput));
 usdInput.addEventListener('input', () => converter(usdInput, somInput, euroInput));
 euroInput.addEventListener('input', () => converter(euroInput, somInput, usdInput));
+
+/// CARD SWITCHER
+
+const btnNext = document.querySelector('#btn-next')
+const btnPrev = document.querySelector('#btn-prev')
+const cardBlock = document.querySelector('.card')
+const maxId = 200
+let cardId = 1
+
+function fetchCard(cardId = 1) {
+    try {
+        fetch(`https://jsonplaceholder.typicode.com/todos/${cardId}`)
+            .then(response => response.json())
+            .then(data => {
+                cardBlock.innerHTML = `
+                      <p>${data.title}</p>
+                      <p>${data.completed}</p>
+                      <span>${data.id}</span>
+                `;
+            })
+    }catch (error) {
+        console.error(error)
+    }
+}
+const upNext = () => {
+    try {
+        if (cardId >= maxId) {
+            cardId = 1
+        }else {
+            cardId++
+        }
+        fetchCard(cardId)
+    }catch (error) {
+        console.error(error)
+    }
+}
+const prev = () => {
+    try {
+        if (cardId <= 1) {
+            cardId = maxId
+        }else {
+            cardId--
+        }
+        fetchCard(cardId)
+    }catch (error) {
+        console.error(error)
+    }
+}
+btnNext.onclick = upNext
+btnPrev.onclick = prev
+fetchCard();
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => console.log(data))
