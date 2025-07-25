904dbee32e7b8f5423b4bb202f1c1e4ae131f145
Eldos106 <toktosunoveldos99@gmail.com>
2025-07-25 13:25:05 +0600

    DZ7_4-month

--- js/lesson.js
+++ js/lesson.js
@@ -201,6 +201,7 @@ const prev = () => {
 btnNext.onclick = upNext
 btnPrev.onclick = prev
 fetchCard();
+
 fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json())
     .then(data => console.log(data))
